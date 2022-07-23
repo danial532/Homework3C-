d37b12cf1415293bf0f181e5d9ce96235440a01b
Danial <tretjakov.danial@yandex.ru>
2022-07-23 19:35:45 +0300

    Выполнена 19 задача

---
+++ Task19/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task19
+{}
+    //Напишите программу, которая принимает на вход пятизначное число 
+    //и проверяет, является ли оно палиндромом.
+    //14212 -> нет
+    //12821 -> да
+    //23432 -> да
+    class Program 
+    {
+        public static void Main(string[] args)
+        {
+        Console.Clear();
+        Console.WriteLine ("Введите пятизначное число, попробуте еще раз");
+        string? N = Console.ReadLine();
+        int NLen = N.Length;
+         if (NLen != 5)     
+            {
+             Console.WriteLine ("Это не пятизначное число");
+            }
+        else 
+            {if (N[0] == N[4] & N[1] == N[3])
+            {
+            Console.WriteLine ("Это полиндром!");
+            }
+            else {
+                 Console.WriteLine ("Это НЕ полиндром");
+                 }
+            }
+        }
+    }
